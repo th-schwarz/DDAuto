eba93ef88637978cb4c13cae5a47c576a8bce14b
Thilo Schwarz <thilo.schwarz@googlemail.com>
2021-05-29 21:35:54 +0200

    added default landing page

---
+++ src/main/java/codes/thischwa/autodyn/rest/GreetingContoller.java
@@ -0,0 +1,24 @@
+package codes.thischwa.autodyn.rest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GreetingContoller {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public ResponseEntity<String> greeting() throws IOException {
+		try (BufferedReader buffer = new BufferedReader(
+				new InputStreamReader(this.getClass().getResourceAsStream("/templates/default.html")))) {
+			return ResponseEntity.ok(buffer.lines().collect(Collectors.joining("\n")));
+		}
+	}
+}
