8cd746de6acf7df317d6813a5d16698727c98bb8
Thilo Schwarz <thilo.schwarz@googlemail.com>
2021-07-02 16:46:18 +0200

    remove validation for test

--- src/test/java/codes/thischwa/autodyn/TestApp.java
+++ src/test/java/codes/thischwa/autodyn/TestApp.java
@@ -1,20 +1,14 @@
 package codes.thischwa.autodyn;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication()
-@Configuration
 public class TestApp {
 
-	@Autowired
-	private AutoDynContext context;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(AutoDynStarter.class, args);
 	}
@@ -24,9 +18,7 @@ public class TestApp {
 		return new ApplicationListener<ApplicationReadyEvent>() {
 			
 			@Override
-			public void onApplicationEvent(ApplicationReadyEvent event) {
-				context.readAndValidateData();	
-				
+			public void onApplicationEvent(ApplicationReadyEvent event) {				
 			}
 		};
 	}
