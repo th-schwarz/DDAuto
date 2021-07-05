7b06ede00f899ea628c66f7d2d1408fc9f549a7f
Thilo Schwarz <thilo.schwarz@googlemail.com>
2021-07-05 14:57:30 +0200

    cleaned-up greeting

--- src/main/java/codes/thischwa/autodyn/GreetingContoller.java
+++ src/main/java/codes/thischwa/autodyn/GreetingContoller.java
@@ -1,12 +1,8 @@
 package codes.thischwa.autodyn;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class GreetingContoller {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
-	public ResponseEntity<String> greeting() throws IOException {
-		try (BufferedReader buffer = new BufferedReader(
-				new InputStreamReader(this.getClass().getResourceAsStream("/templates/default.html")))) {
-			return ResponseEntity.ok(buffer.lines().collect(Collectors.joining("\n")));
-		}
+	public String greeting() throws IOException {
+		return "about.html";
 	}
 }
