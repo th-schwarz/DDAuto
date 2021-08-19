d9f32deddfcbcae34c1d833614bf67a93ce5ae3c
Thilo Schwarz <thilo.schwarz@googlemail.com>
2021-08-19 19:14:35 +0200

    added tests

--- src/test/java/codes/thischwa/ddauto/DDAutoContextTest.java
+++ src/test/java/codes/thischwa/ddauto/DDAutoContextTest.java
@@ -15,6 +15,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(classes = { DDAutoConfig.class, DDAutoContext.class })
 @ExtendWith(SpringExtension.class)
 class DDAutoContextTest {
+	
+	private final int configuredEntries = 2;
 
 	@Autowired
 	private DDAutoContext context;
@@ -27,13 +29,25 @@ class DDAutoContextTest {
 	@Test
 	final void testGetApiToken() {
 		assertEquals("1234567890abcdef", context.getApitoken("my0.dynhost0.info"));
+		assertThrows(IllegalArgumentException.class, () -> {
+			context.getApitoken("unknown.host.info");
+		});
 	}
 
 	@Test
 	final void testgetPrimaryNameServer() {
 		assertEquals("ns1.domain.info", context.getPrimaryNameServer("dynhost1.info"));
+		assertThrows(IllegalArgumentException.class, () -> {
+			context.getPrimaryNameServer("unknown-host.info");
+		});
 	}
 
+	@Test
+	final void testConfigured() {
+		assertEquals(configuredEntries*2, context.getConfiguredHosts().size());
+		assertEquals(configuredEntries, context.getConfiguredZones().size());
+	}
+	
 	@Test
 	final void testValidateData_ok() {
 		context.validateData(Map.of("domain.tld", "ns.nameserver.tld"), Map.of("sld.domain.tld", "1234567890abcdf"));
