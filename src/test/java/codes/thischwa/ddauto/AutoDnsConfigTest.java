1e68a7e0b25607c887bcdb26e4571c72d49a2ab6
Thilo Schwarz <thilo.schwarz@googlemail.com>
2021-11-05 15:35:38 +0100

    revert fix #4

--- src/test/java/codes/thischwa/ddauto/AutoDnsConfigTest.java
+++ src/test/java/codes/thischwa/ddauto/AutoDnsConfigTest.java
@@ -1,6 +1,6 @@
 package codes.thischwa.ddauto;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
