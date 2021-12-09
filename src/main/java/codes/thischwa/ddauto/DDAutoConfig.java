29b11357d00e21ef0b7587f1e59907ae279632e5
Thilo Schwarz <thilo.schwarz@googlemail.com>
2021-12-09 16:08:51 +0100

    added bean validation for the Zone

--- src/main/java/codes/thischwa/ddauto/DDAutoConfig.java
+++ src/main/java/codes/thischwa/ddauto/DDAutoConfig.java
@@ -2,7 +2,9 @@ package codes.thischwa.ddauto;
 
 import java.util.List;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,24 +15,26 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "ddauto")
 public class DDAutoConfig {
 
+	@NotEmpty(message = "The zones of the AutoDNS configuration shouldn't be empty.")
 	private List<Zone> zones;
 
 	public List<Zone> getZones() {
 		return zones;
 	}
 
-	public void setZones(List<Zone> zones) {
+	public void setZones(List<@Valid Zone> zones) {
 		this.zones = zones;
 	}
 
 	public static class Zone {
 
-		@NotBlank
+		@NotBlank(message = "The name of the zone shouldn't be empty.")
 		private String name;
 
-		@NotBlank
+		@NotBlank(message = "The primary name server of the zone shouldn't be empty.")
 		private String ns;
 
+		// is validated by DDAutoContext#readData
 		private List<String> hosts;
 
 		public String getName() {
@@ -53,7 +57,7 @@ public class DDAutoConfig {
 			return hosts;
 		}
 
-		public void setHosts(List<String> host) {
+		public void setHosts(@Valid List<String> host) {
 			this.hosts = host;
 		}
 
---
+++ src/test/java/codes/thischwa/ddauto/DDAutoConfigValidationTest.java
@@ -0,0 +1,73 @@
+package codes.thischwa.ddauto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(classes = { DDAutoConfig.class })
+@ExtendWith(SpringExtension.class)
+class DDAutoConfigValidationTest {
+
+	@Autowired
+	private DDAutoConfig config;
+		private static Validator validator;
+
+
+	@BeforeAll
+	public static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	final void testZones() {
+        Set<ConstraintViolation<DDAutoConfig>> violations = validator.validate(config);
+        assertTrue(violations.isEmpty());
+		assertEquals(2, config.getZones().size());
+	}
+
+	@Test
+	final void testZone_failName() {
+		DDAutoConfig.Zone z = buildZone();
+		z.setName(null);
+        Set<ConstraintViolation<DDAutoConfig.Zone>> violations = validator.validate(z);
+        assertEquals(1, violations.size());
+        assertEquals("The name of the zone shouldn't be empty.", violations.iterator().next().getMessage());
+	}
+	
+	@Test
+	final void testZone_failHost() {
+		DDAutoConfig.Zone z = buildZone();
+		z.setHosts(Arrays.asList("test1", "test2", ""));
+        Set<ConstraintViolation<DDAutoConfig.Zone>> violations = validator.validate(z);
+        assertEquals(1, violations.size());
+        assertEquals("Host shouldn't be blank.", violations.iterator().next().getMessage());		
+        
+        z.setHosts(null);
+        violations = validator.validate(z);
+        assertEquals(1, violations.size());
+        assertEquals("The hosts of the zone shouldn't be empty.", violations.iterator().next().getMessage());		
+	}
+	
+	final static DDAutoConfig.Zone buildZone() {
+		DDAutoConfig.Zone z = new DDAutoConfig.Zone();
+		z.setName("test.dyndns.org");
+		z.setNs("ns.dyndns.org");
+		z.setHosts(Arrays.asList("test1", "test2"));
+		return z;
+	}
+}
