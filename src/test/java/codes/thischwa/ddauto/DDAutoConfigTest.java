cd339e750d0291fb9ba183cb31f9b1942e96f05a
Thilo Schwarz <osp@thischwa.codes>
2022-01-01 13:04:37 +0100

    merged DDAutoConfigFailTest with DDAutoConfigTest

--- src/test/java/codes/thischwa/ddauto/DDAutoConfigTest.java
+++ src/test/java/codes/thischwa/ddauto/DDAutoConfigTest.java
@@ -2,6 +2,9 @@ package codes.thischwa.ddauto;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -65,4 +68,25 @@ class DDAutoConfigTest {
 		config.validateData();
 	}
 
+	@Test
+	final void testWrongHostFormat() {
+		String wrongHost = "wrong-formatted.host";
+		DDAutoConfig.Zone z = config.getZones().get(0);
+		z.getHosts().add(wrongHost);
+		assertThrows(IllegalArgumentException.class, () -> {
+			config.readData();
+		});
+		z.getHosts().remove(wrongHost);
+	}
+
+	@Test
+	final void testEmptyHosts() {
+		DDAutoConfig.Zone z = config.getZones().get(1);
+		List<String> hosts = new ArrayList<>(z.getHosts());
+		z.getHosts().clear();
+		assertThrows(IllegalArgumentException.class, () -> {
+			config.readData();
+		});
+		z.setHosts(hosts);
+	}
 }
