30a3a4f79bbf46dd6d2cc001a2e106978f3fc5fa
Thilo Schwarz <osp@thischwa.codes>
2022-01-23 18:57:19 +0100

    wip #16, add dynamic logback config

--- src/main/java/codes/thischwa/ddauto/DDAutoStarter.java
+++ src/main/java/codes/thischwa/ddauto/DDAutoStarter.java
@@ -1,5 +1,8 @@
 package codes.thischwa.ddauto;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,15 +20,23 @@ public class DDAutoStarter {
 
 	private static final String const_swagger_enabled_cli = "--swagger.enabled=true";
 	
+	private static String workingDir = System.getProperty("user.dir");
+	
+	private static final String const_logback_name = "logback.xml";
+	
 	public static void main(String[] args) {
 		try {
 			SpringApplication app = new SpringApplication(DDAutoStarter.class);
-
+ 
 			List<String> cmdArgs = new ArrayList<>(Arrays.asList(args));
-			// map swagger property
+			// mapswagger property
 			if(!cmdArgs.remove(const_swagger_enabled_cli)) 
 				cmdArgs.add("--springdoc.api-docs.enabled=false");
 			
+			// logback config
+			Path logConfPath = Paths.get(workingDir, const_logback_name);
+			if(Files.exists(logConfPath))
+				cmdArgs.add("--logging.config=" + logConfPath.toFile().getAbsolutePath());
 			app.run(cmdArgs.toArray(new String[cmdArgs.size()]));
 		} catch (Exception e) {
 			System.err.println("Unexpected exception, Spring Boot stops! Message: " + e.getMessage());
