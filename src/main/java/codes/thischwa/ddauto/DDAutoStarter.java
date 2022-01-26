993ea13d9e93f30f2a0acca6f5e4ba9f25babecc
Thilo Schwarz <osp@thischwa.codes>
2022-01-26 12:47:58 +0100

    cleaned up

--- src/main/java/codes/thischwa/ddauto/DDAutoStarter.java
+++ src/main/java/codes/thischwa/ddauto/DDAutoStarter.java
@@ -2,20 +2,17 @@ package codes.thischwa.ddauto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * The starter and config.
  */
 @SpringBootApplication
-@Configuration
 public class DDAutoStarter {
 
 	
 	public static void main(String[] args) {
 		try {
-			SpringApplication app = new SpringApplication(DDAutoStarter.class);
-			app.run(CommandlineArgsProcessor.process(args).toArray(new String[0]));
+			SpringApplication.run(DDAutoStarter.class, CommandlineArgsProcessor.process(args).toArray(new String[0]));
 		} catch (Exception e) {
 			System.err.println("Unexpected exception, Spring Boot stops! Message: " + e.getMessage());
 			//System.exit(10);
