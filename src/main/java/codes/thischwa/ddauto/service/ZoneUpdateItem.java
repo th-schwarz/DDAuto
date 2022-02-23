8e578da805059d6218087dc525b6404a5225e0a6
Thilo Schwarz <osp@thischwa.codes>
2022-02-23 19:35:45 +0100

    worked on #17

--- src/main/java/codes/thischwa/ddauto/service/ZoneUpdateItem.java
+++ src/main/java/codes/thischwa/ddauto/service/ZoneUpdateItem.java
@@ -1,29 +1,23 @@
 package codes.thischwa.ddauto.service;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 public class ZoneUpdateItem {
 
-	@JsonIgnore
 	private String dateTime;
 
-	@JsonIgnore
 	private String host;
 
-	@JsonIgnore	
 	private String ipv4;
 
-	@JsonIgnore
 	private String ipv6;
 	
-	private String item;
+	private String cvs;
 		
 	ZoneUpdateItem(String dateTime, String host, String ipv4, String ipv6) {
 		this.dateTime = dateTime;
 		this.host = host;
 		this.ipv4 = ipv4 == null ? "n/a" : ipv4;
 		this.ipv6 = ipv6  == null ? "n/a" : ipv6;
-		this.item = String.format("%s,%s,%s,%s", dateTime, host, ipv4, ipv6);
+		this.cvs = String.format("%s,%s,%s,%s", dateTime, host, ipv4, ipv6);
 	}
 
 	public String getDateTime() {
@@ -42,8 +36,8 @@ public class ZoneUpdateItem {
 		return ipv6;
 	}
 	
-	public String getItem() {
-		return item;
+	public String getCvs() {
+		return cvs;
 	}
 
 	@Override
