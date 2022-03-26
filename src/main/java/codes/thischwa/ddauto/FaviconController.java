a3c0093169c2489bfc186edae19c9237075e3987
Thilo Schwarz <osp@thischwa.codes>
2022-03-26 09:52:14 +0100

    initial commit

---
+++ src/main/java/codes/thischwa/ddauto/FaviconController.java
@@ -0,0 +1,14 @@
+package codes.thischwa.ddauto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FaviconController {
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
+    }
+}
