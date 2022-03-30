fde55ec9ce5b5c73e727e91a23de9f30e2ff317f
Thilo Schwarz <osp@thischwa.codes>
2022-03-30 20:18:00 +0200

    ignore for OAS

--- src/main/java/codes/thischwa/ddauto/FaviconController.java
+++ src/main/java/codes/thischwa/ddauto/FaviconController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @Controller
 public class FaviconController {
 
+	@Operation(hidden = true)
     @GetMapping("favicon.ico")
     @ResponseBody
     void returnNoFavicon() {
