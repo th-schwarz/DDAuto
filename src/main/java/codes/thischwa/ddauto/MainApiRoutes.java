ba48e062b44805f31b512faf14b08525b337139f
Thilo Schwarz <osp@thischwa.codes>
2022-04-02 19:51:47 +0200

    moved OpenAPIDefinition to MainApiRoutes, enabled pretty print

--- src/main/java/codes/thischwa/ddauto/MainApiRoutes.java
+++ src/main/java/codes/thischwa/ddauto/MainApiRoutes.java
@@ -1,23 +1,41 @@
 package codes.thischwa.ddauto;
 
-import javax.servlet.http.HttpServletRequest;
-
+import codes.thischwa.ddauto.service.ZoneLogPage;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import codes.thischwa.ddauto.service.ZoneLogPage;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.servlet.http.HttpServletRequest;
 
-@Tag(name = "main", description = "The routes.")
+//@formatter:off
+@OpenAPIDefinition(
+		info = @Info(
+				title = "DDAuto :: Dynamic DNS with AutoDNS",
+				description = "The routes of the dynamic DNS API",
+				version = "1.0", 
+				license = @License(
+						name = "MIT Licence", 
+						url = "https://github.com/th-schwarz/DDAuto/blob/develop/LICENSE")),
+		externalDocs = @ExternalDocumentation(
+				description = "DDAuto on Github",
+				url = "https://github.com/th-schwarz/DDAuto"
+		)
+)
+@Tag(name = "main", description = "The main api routes.")
+//@formatter:on
 public interface MainApiRoutes {
 
 	@Operation(summary = "Checks, if the 'host' exists and is configured.")
@@ -32,7 +50,6 @@ public interface MainApiRoutes {
 					@ExampleObject(value = "mydyndns.domain.com") }))
 			@PathVariable String host);
 
-
 	@Operation(summary = "Updates the desired IP addresses of the 'host', if the 'apitoken' belongs to the host. If both parameters for IP addresses aren't set, an attempt is made to fetch the remote IP.")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Updates the desired IP addresses of the 'host', if the 'apitoken' belongs to the host. If both parameters for IP addresses aren't set, an attempt is made to fetch the remote IP.", content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, examples = {
@@ -46,7 +63,6 @@ public interface MainApiRoutes {
 					@ExampleObject(value = "mydyndns.domain.com") }))
 			@PathVariable String host, @RequestParam String apitoken, @RequestParam(name = "ipv4", required = false) String ipv4Str,
 			@RequestParam(name = "ipv6", required = false) String ipv6Str, HttpServletRequest req);
-
 
 	@Operation(summary = "Determine the IP settings of the 'host' and returns it as formatted plain text.")
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Formatted plain text with the IP settings of the 'host'"),
@@ -67,6 +83,6 @@ public interface MainApiRoutes {
 
 	@Operation(summary = "Zone update log.")
 	@ApiResponse(responseCode = "200", description = "All zone update log.")
-	@GetMapping(value = "info/zone-log", produces = MediaType.APPLICATION_JSON_VALUE)	
+	@GetMapping(value = "info/zone-log", produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ZoneLogPage> getZoneUpdateLogs();
 }
