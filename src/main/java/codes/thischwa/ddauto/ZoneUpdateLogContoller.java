20bd1ab46d56a470b8a1a1dfdbc9d8c69ee2af9a
Thilo Schwarz <osp@thischwa.codes>
2022-05-15 12:58:31 +0200

    removed obsolete route

--- src/main/java/codes/thischwa/ddauto/ZoneUpdateLogContoller.java
+++ src/main/java/codes/thischwa/ddauto/ZoneUpdateLogContoller.java
@@ -45,8 +45,4 @@ public class ZoneUpdateLogContoller {
 		return "log-zone";
 	}
 
-	@GetMapping(value = "/log-ex", produces = MediaType.TEXT_HTML_VALUE)
-	public String logEx(Model model) {
-		return "log-ex.html";
-	}
 }
