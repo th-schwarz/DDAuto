1555c1223104752d787f1d26eb4a2c0967158083
Thilo Schwarz <osp@thischwa.codes>
2022-06-17 19:45:12 +0200

    fixed #27, SecurityConfig uses the filter chain of spring security

--- src/main/java/codes/thischwa/ddauto/config/SecurityConfig.java
+++ src/main/java/codes/thischwa/ddauto/config/SecurityConfig.java
@@ -1,40 +1,56 @@
 package codes.thischwa.ddauto.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	@Value("${spring.security.user.name}")
-	private String user;
+	private String userName;
 
 	@Value("${spring.security.user.password}")
 	private String password;
 	
-	@Autowired
 	private DDAutoConfig ddAutoConfig;
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().withUser(user).password(encoder.encode(password)).roles("USER");
-		if(ddAutoConfig.getZoneLogUserName() != null && ddAutoConfig.getZoneLogUserPassword() != null) {
-			auth.inMemoryAuthentication().withUser(ddAutoConfig.getZoneLogUserName()).password(encoder.encode(ddAutoConfig.getZoneLogUserPassword()))
-			.roles("LOGVIEWER");
-		}
+	
+	public SecurityConfig(DDAutoConfig ddAutoConfig) {
+		this.ddAutoConfig = ddAutoConfig;
 	}
+		
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername(userName)
+            .password(password)
+            .roles("USER")
+            .build();
+        InMemoryUserDetailsManager userManager = new InMemoryUserDetailsManager();
+        userManager.createUser(user);
+        if(ddAutoConfig.getZoneLogUserName() != null && ddAutoConfig.getZoneLogUserPassword() != null) {
+			UserDetails logUser = User.withUsername(ddAutoConfig.getZoneLogUserName())
+					.password(ddAutoConfig.getZoneLogUserPassword())
+					.roles("LOGVIEWER")
+					.build();
+			userManager.createUser(logUser);
+		}
+        return userManager;
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@SuppressWarnings("deprecation")
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+      return org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 		// disable security for greeting and open-api endpoint
 		.authorizeRequests().antMatchers("/", "/favicon.ico", "/v3/api-docs*").permitAll().and()
@@ -44,6 +60,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		// other routes
 		.authorizeRequests().anyRequest().hasAnyRole("USER").and().httpBasic();
+		
+		return http.build();
 	}
 	
 }
